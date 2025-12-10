187de8a413c2a03b1f7887aab4455974eb48c143
Kevin De Vijlder <kevin.de.vijlder@protonmail.com>
2025-12-10 23:23:10 +0100

    added backend start

---
+++ backend/server.js
@@ -0,0 +1,16 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// test route
+app.get("/", (req, res) => {
+  res.json({ message: "MediaLedger backend is running" });
+});
+
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log(`Backend running on http://localhost:${PORT}`);
+});
